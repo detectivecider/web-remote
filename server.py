7f8e0269449b15e2ab0cd42e702392458f1f211e
user <>
2025-02-13 18:34:51 +0000

    Adding local ip detection during runtime

--- server.py
+++ server.py
@@ -1,6 +1,33 @@
 import os
 import requests
 from bottle import route, request, static_file, run, redirect
+import fileinput
+import socket
+
+ip = (([[(s.connect(("8.8.8.8", 53)), s.getsockname()[0], s.close()) for s in [socket.socket(socket.AF_INET, socket.SOCK_DGRAM)]][0][1]]) + ["no IP found"])[0]
+ip_js = "'http://" + ip + ":8080/'"
+
+file = open('assets/js/script.js')
+def findip():
+    for line in file:
+        if "http" in line:
+            for old_ip in line.strip().split():
+                if "http" in old_ip:
+                    return old_ip
+old_ip = findip()
+for line in fileinput.input('assets/js/script.js', inplace = 1):
+    print(line.replace(old_ip, ip_js).strip())
+
+ #for item in files:
+#    if "http" in item:
+#        exp = findip()
+#        print(str(item).replace(exp,ip), end="")
+#    else:
+#        print(str(item), end="")
+
+#for line in fileinput.input(files, inplace = 1): 
+#      print(line.replace("foo", "bar")),
+
 
 @route('/')
 def root():
@@ -8,14 +35,18 @@ def root():
 
 @route('/css/bulma.css')
 def bulma():
-    return static_file('bulma.css', root='css')
+    return static_file('bulma.css', root='assets/css')
 @route('/css/sheet.css')
 def sheet():
-    return static_file('sheet.css', root='css')
+    return static_file('sheet.css', root='assets/css')
 
 @route('/assets/remote-control.png')
 def favi():
     return static_file('remote-control.png', root='assets')
+
+@route('/js/script.js')
+def favi():
+    return static_file('script.js', root='assets/js')
 
 @route('/upload', method='POST')
 def do_upload():
@@ -30,14 +61,16 @@ def do_upload():
         os.makedirs(save_path)
 
     file_path = "{path}/{file}".format(path=save_path, file=upload.filename)
-
+    ip_path=('http://'+ip+':8080/open-pdf?file='+file_path)
+    ip_front=('http://'+ip+':9999')
     if os.path.exists(file_path):
-        requests.get('http://192.168.1.19:8080/open-pdf?file={file_path}'.format(file_path=file_path))
-        redirect('http://192.168.1.19:9999')
+        requests.get(ip_path)
+        redirect(ip_front)
     else:
         upload.save(file_path)
-        requests.get('http://192.168.1.19:8080/open-pdf?file={file_path}'.format(file_path=file_path))
-        redirect('http://192.168.1.19:9999')
+        requests.get(ip_path)
+
+        redirect(ip_front)
 
 
 if __name__ == '__main__':
