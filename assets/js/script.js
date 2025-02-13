0d0e37692546bbc6799806fdcb13a11489fe0661
user <>
2025-02-13 18:35:38 +0000

    Editing Default IP

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 function submit(page) {
 var xhr = new XMLHttpRequest();
-xhr.open('get', 'http://192.168.159.86:8080/' + page, true);
+xhr.open('get', 'http://0.0.0.0:8080/' + page, true);
 xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded; charset=UTF-8');
 xhr.send();
 }
