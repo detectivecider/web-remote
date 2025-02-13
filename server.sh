9d7759a9f275ac09d85a34ad52631e28071ea2ab
user <>
2025-02-13 18:34:32 +0000

    Switching to dotool

--- server.sh
+++ server.sh
@@ -1,12 +1,14 @@
-bin/shell2http -export-vars=DISPLAY -form \
-    /fullscreen "xdotool key f" \
-    /move-up "xdotool key k" \
-    /move-down "xdotool key j" \
-    /move-right "xdotool key l" \
-    /move-left "xdotool key h" \
-    /zoom "xdotool key plus" \
-    /zoom-out "xdotool key minus" \
-    /page-up "xdotool key Prior" \
-    /page-down "xdotool key Next" \
-    /open-pdf 'mupdf $v_file' \
-    /quit "xdotool key q"
+shell2http -export-all-vars -form \
+	/fullscreen "echo 'key f' | dotool" \
+    /move-up "echo 'key k' | dotool" \
+    /move-down "echo 'key j' | dotool" \
+    /move-right "echo key l | dotool" \
+    /move-left "echo 'key h' | dotool" \
+    /zoom "echo 'key k:78' | dotool" \
+    /zoom-out "echo 'key k:12' | dotool" \
+    /page-up "echo 'key k:104' | dotool" \
+    /page-down "echo 'key k:109' | dotool" \
+    /open-pdf 'zathura $v_file' \
+    /quit "echo 'key q' | dotool" &
+
+python server.py &
